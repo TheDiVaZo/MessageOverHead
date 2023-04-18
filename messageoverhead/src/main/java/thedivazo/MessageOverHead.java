c729464e9c200864d9247d80c23a891ce7076b6e
Dima Klovnykh <70421596+TheDiVaZo@users.noreply.github.com>
2023-04-18 07:22:49 +0300

    Update version

--- messageoverhead/src/main/java/thedivazo/MessageOverHead.java
+++ messageoverhead/src/main/java/thedivazo/MessageOverHead.java
@@ -41,7 +41,7 @@ import java.util.stream.Collectors;
 @Author(value = "TheDiVaZo")
 @ApiVersion(value = ApiVersion.Target.v1_13)
 public class MessageOverHead extends JavaPlugin {
-    public static final String VERSION = "3.3";
+    public static final String VERSION = "3.4";
     public static final String NAME = "MessageOverHead";
 
     private static ConfigManager configManager;
